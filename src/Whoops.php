0b9513a80a0325d7d151d260642a1325d0d52f60
Gabriel <gabrielmanica@hotmail.com.br>
2022-09-02 17:43:42 -0300

    echo

--- src/Whoops.php
+++ src/Whoops.php
@@ -1,7 +1,7 @@
 <?php
 $ini = parse_ini_file('app/config/application.ini', true);
 
-if (!empty(self::$ini['general']['debug']) && $ini['general']['debug'] == '1')
+if (!empty($ini['general']['debug']) && $ini['general']['debug'] == '1')
 {
     $handler = new \Whoops\Handler\PrettyPageHandler;
     $handler->setEditor("vscode");
