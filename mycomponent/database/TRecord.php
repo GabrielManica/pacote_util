31e4737c3c3d673289c45069d9bb0ce03340b107
Gabriel <gabrielmanica@hotmail.com.br>
2022-09-30 15:25:13 -0300

    TRecord

---
+++ mycomponent/database/TRecord.php
@@ -0,0 +1,16 @@
+<?php
+namespace Mycomponent\Database;
+
+class TRecord extends \Adianti\Database\TRecord
+{
+    public function default_values(){
+
+    }
+
+    public function onAfterLoad($object)
+    {
+        foreach ($object as $key => $value) {
+            $object->$key = trim($value);
+        }
+    }
+}
