cf157b3baa40b9f4f5250cecd7f492ac4b0200bb
Gabriel <gabrielmanica@hotmail.com.br>
2022-09-30 10:22:08 -0300

    remove class TDBCombo

--- mycomponent/control/TPage.php
+++ mycomponent/control/TPage.php
@@ -3,21 +3,11 @@ namespace Mycomponent\Control;
 
 use Adianti\Registry\TSession;
 
-if (!defined('conexao')) {
-    define('conexao', TSession::getValue('conexao'));
+if (!defined('CONEXAO')) {
+    define('CONEXAO', TSession::getValue('conexao'));
 }
 
 class TPage extends \Adianti\Control\TPage
 {
-    protected static $database = conexao;
-
-    // public function __construct()
-    // {
-    //     parent::__construct('div');
-    //     $this->constructed = TRUE;
-
-    //     $this->{'page-name'} = $this->getClassName();
-    //     $this->{'page_name'} = $this->getClassName();
-    // }
-
+    protected static $database = CONEXAO;
 }
