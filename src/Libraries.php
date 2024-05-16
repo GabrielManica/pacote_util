395176c2585c62a42645d090ce9d846740c1cd51
Gabriel <gabrielmanica@hotmail.com.br>
2024-05-16 08:30:28 -0300

    novos componentes

---
+++ src/Libraries.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<script src="vendor/gabrielmanica/pacote_util/mycomponent/include/components.min.js?afver=1" type="text/javascript"></script>';
+echo '<script src="vendor/gabrielmanica/pacote_util/independent/js/independent-plugins.min.js?afver=310" type="text/javascript"></script>';
+echo '<link href="vendor/gabrielmanica/pacote_util/mycomponent/include/components.min.css?afver=1" rel="stylesheet" type="text/css" media="screen" />';
+echo '<link href="vendor/gabrielmanica/pacote_util/independent/css/independent-plugins.min.css?afver=934" rel="stylesheet" type="text/css" media="screen" />';
